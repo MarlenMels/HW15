29ec35d0f846b9350897bca5a882399d765e405b
Marlen Mels <marlenmels9@gmail.com>
2024-09-15 22:02:22 +0500

    Задание сделанно

--- Task1/src/Main.java
+++ Task1/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.HashSet;
+
 public class Main {
     //todo Напишите код который распечатает букву и ее код для всех букв русского алфавита формат вывода:
     // А-123
@@ -5,5 +7,17 @@ public class Main {
     // Коды даны для примера и не соответствуют действительности. Учитывайте, что нужно распечатать коды букв в двух регистрах!
     public static void main(String[] args) {
         //todo код писать тут
+
+        for(int i = 1040; i <= 1045; i++){
+            System.out.println((char)i + "-" + i);
+        }
+        System.out.println('Ё' + "-" + (int)'Ё');
+        for(int i = 1046; i <= 1077; i++){
+            System.out.println((char)i + "-" + i);
+        }
+        System.out.println('ё' + "-" + (int)'ё');
+        for(int i = 1078; i <= 1103; i++){
+            System.out.println((char)i + "-" + i);
+        }
     }
 }
