0c5ab70bd502d77dacc2d4c22af90ab018bfccb3
Marlen Mels <marlenmels9@gmail.com>
2024-09-17 09:48:45 +0500

    I done it

--- Task2/src/Main.java
+++ Task2/src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
+    public static final String CORRECT_CHARS = " АБВГДЕЁЖЗКИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя";
     public static void main(String[] args) {
-        //todo код писать тут. По желанию(рекомендуется) вынести код в методы.
+        String FIO = new Scanner(System.in).nextLine();
+        check(FIO);
+    }
+
+    public static void check(String FIO){
+        boolean isValid = true;
+        int spaceCount = 0;
+        for(int i = 0; i<FIO.length(); i++){
+            if(FIO.charAt(i) == ' '){
+                spaceCount++;
+            }
+            if(!CORRECT_CHARS.contains(String.valueOf(FIO.charAt(i)))){
+                isValid = false;
+                break;
+            }
+        }
+
+        if(isValid && spaceCount == 2) {
+            System.out.println(FIO.substring(FIO.indexOf(' ')).trim());
+        } else {
+            System.out.println("Введенная строка не является ФИО");
+        }
     }
 }
