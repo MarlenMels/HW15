c7f1ffd291cad2395c49ff70f7bc163cba2a7aa3
Marlen Mels <marlenmels9@gmail.com>
2024-09-18 09:39:58 +0500

    Completely done the task

--- Task2/src/Main.java
+++ Task2/src/Main.java
@@ -4,26 +4,32 @@ public class Main {
     public static final String CORRECT_CHARS = " АБВГДЕЁЖЗКИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя";
     public static void main(String[] args) {
         String FIO = new Scanner(System.in).nextLine();
-        check(FIO);
+        System.out.println(check(FIO));
     }
 
-    public static void check(String FIO){
-        boolean isValid = true;
+    public static String check(String FIO){
+        boolean isValid = false;
         int spaceCount = 0;
-        for(int i = 0; i<FIO.length(); i++){
-            if(FIO.charAt(i) == ' '){
+        String output = FIO.trim();
+        String ans = "";
+        for(int i = 0; i<output.length(); i++){
+            if(output.charAt(i) == ' '){
                 spaceCount++;
             }
-            if(!CORRECT_CHARS.contains(String.valueOf(FIO.charAt(i)))){
-                isValid = false;
+            if(!CORRECT_CHARS.contains(String.valueOf(output.charAt(i)))){
+                isValid = true;
                 break;
             }
         }
 
         if(isValid && spaceCount == 2) {
-            System.out.println(FIO.substring(FIO.indexOf(' ')).trim());
+            String lastName = output.substring(0, output.indexOf(' ')).trim();
+            String firstName = output.substring(output.indexOf(' '), output.lastIndexOf(' ')).trim();
+            String fatherName = output.substring(output.lastIndexOf(' ')).trim();
+            ans = "Фамилия: " + lastName + "\nИмя: " + firstName + "\nОтчество: " + fatherName;
         } else {
             System.out.println("Введенная строка не является ФИО");
         }
+        return ans;
     }
 }
